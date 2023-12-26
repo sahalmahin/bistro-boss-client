1a3343f9b85b90ef03689cfcac4463efb41c5e3e
sahalmahin <samirsatter0142@gmail.com>
2023-12-27 00:32:25 +0600

    swiper installed

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar fixed z-10 bg-base-100">
+        <div className="navbar fixed z-10 max-w-screen-xl bg-opacity-30 bg-black text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -16,7 +16,7 @@ const Navbar = () => {
                         {navOptions}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Bistro Boss</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -24,7 +24,7 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+            <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
         </div>
     );
