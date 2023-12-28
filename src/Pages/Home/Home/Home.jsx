9b8b8aa2939189ab9ab5042924b7f47a92de43eb
sahalmahin <samirsatter0142@gmail.com>
2023-12-29 01:15:49 +0600

    cover integrated

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,15 +3,21 @@ import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testimonials/Testimonials";
+import Footer from "../../../Pages/Shared/Footer/Footer";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Home</title>
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <PopularMenu></PopularMenu>
             <Featured></Featured>
             <Testimonials></Testimonials>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,14 @@
+import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg"
+
 const Menu = () => {
     return (
         <div>
-<h3>Menu is coming</h3>            
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
+            <Cover img={menuImg}></Cover>
         </div>
     );
 };
