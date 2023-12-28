8a31f6286e4701f493a9966e5f0bbe26f1b98e6f
sahalmahin <samirsatter0142@gmail.com>
2023-12-29 03:01:25 +0600

    fiter out offerd items

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,15 +1,31 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg"
+import useMenu from "../../../Hooks/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+
+    const menu = useMenu();
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const salad = menu.filter(item => item.category === 'salad');
+    const offered = menu.filter(item => item.category === 'offered');
+    console.log(offered);
+
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImg} title="Our Menu"></Cover>
-
+            {/* main cover */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"
+            ></SectionTitle>
+            {/* offered menu items */}
+            <MenuCategory items={offered}></MenuCategory>
         </div>
     );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,8 +1,19 @@
-const MenuCategory = () => {
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+const MenuCategory = ({ items, title, coverImg }) => {
     return (
-        <div>
-            
+       <div>
+        {title && <Cover img={coverImg} title="Our Menu"></Cover>}
+        <div className="grid md:grid-cols-2 gap-10 mb-12">
+            {
+               items.map(item => <MenuItem 
+                key={item._id} 
+                item={item}
+                ></MenuItem>)
+            }
         </div>
+       </div>
     );
 };
 
