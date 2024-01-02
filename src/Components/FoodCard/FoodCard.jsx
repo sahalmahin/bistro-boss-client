aa5ef16e9fd977a839368182f0031d61e58c8b82
sahalmahin <samirsatter0142@gmail.com>
2024-01-02 12:19:12 +0600

    save cart data

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,6 +1,7 @@
 import Swal from "sweetalert2";
 import useAuth from "../../Hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const FoodCard = ({ item }) => {
 
@@ -23,6 +24,15 @@ const FoodCard = ({ item }) => {
             axios.post('http://localhost:5000/carts', cartItem)
                 .then(res => {
                     console.log(res.data);
+                    if(res.data.insertedId){
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: `${name} added to your cart`,
+                            showConfirmButton: false,
+                            timer: 1500
+                          });
+                    }
                 })
         } else {
             Swal.fire({
